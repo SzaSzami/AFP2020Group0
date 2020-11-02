5350ab58ea8c31684a39d1f6ebe902a35e3669e4
DBotond <dbotond89@gmail.com>
2020-11-02 10:45:09 +0100

    dao javítva

--- backend/src/main/java/hu/uni/eku/tzs/dao/entity/Guest.java
+++ backend/src/main/java/hu/uni/eku/tzs/dao/entity/Guest.java
@@ -19,5 +19,5 @@ public class Guest {
     @Column
     private String name;
     @Column
-    private int Age;
+    private int age;
 }
--- backend/src/main/java/hu/uni/eku/tzs/model/Guest.java
+++ backend/src/main/java/hu/uni/eku/tzs/model/Guest.java
@@ -9,6 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Guest {
 
-    private String name();
+    private String name;
     private int age;
 }
